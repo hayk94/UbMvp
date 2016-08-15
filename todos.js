99ee86505a3ba9f0e60e72a515c5cdd2ed44555b
Hayk <hayk94@gmail.com>
2016-08-15 11:07:39 +0400

    added todos

---
+++ todos.js
@@ -0,0 +1,7 @@
+// TODO: MVP GOALS
+        //1 TODO: Get a list of users who attended the site.
+        //2 TODO: See what pages they attended and their order as a timeline.
+        //3 TODO: See what links,buttons the user clicked.
+        //4 TODO: If the user is online see at what page he's now.
+//TODAY
+        // TODO: research google and plan
