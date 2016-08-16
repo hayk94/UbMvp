ff27febaa8e687efdd1e63abfca662f75192e170
Hayk <hayk94@gmail.com>
2016-08-16 11:14:25 +0400

    successfully created another db

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,15 @@
 import { Template } from 'meteor/templating';
 
 import { Tasks } from '../api/tasks.js';
+import { Conns } from '../api/conns.js';
 
 import "./body.html";
 
 Template.body.helpers({
   tasks(){
     return Tasks.find({});
-  }
+  },
+  conns(){
+    return Conns.find({});
+  },
 });
