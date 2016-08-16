d674df258b040a01d3126ebc413ee4c2ffd16f3b
Hayk <hayk94@gmail.com>
2016-08-16 15:32:45 +0400

    added the connection

--- server/main.js
+++ server/main.js
@@ -2,4 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Meteor.onConnection(function(conn){
+    //  console.log(conn);
+    
+  });
 });
