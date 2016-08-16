ec667c5a36ae253c4cbf1c4f8338a3bb4d593d65
Hayk <hayk94@gmail.com>
2016-08-16 11:16:51 +0400

    implementing onConnection

--- server/main.js
+++ server/main.js
@@ -5,4 +5,11 @@ import '../imports/api/conns.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Meteor.onConnection(function(conn){
+      // console.log(conn);
+      connInfoS.insert({
+        connID: conn.id,
+        ipAdr: conn.clientAddress,
+        httpHeads: conn.httpHeaders,
+      });
 });
