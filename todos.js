bdf7febeb5f10662e38bdd15c572441b6d20d6a7
Hayk <hayk94@gmail.com>
2016-08-16 10:15:56 +0400

    todos

--- todos.js
+++ todos.js
@@ -3,9 +3,12 @@
         //2 TODO: See what pages they attended and their order as a timeline.
         //3 TODO: See what links,buttons the user clicked.
         //4 TODO: If the user is online see at what page he's now.
-//TODAY
+//CURRENT
         // TODO: research google and plan
           // XXX: meteor-user-status seems to be a bad idea
-          
-          // TODO: check https://docs.meteor.com/api/connections.html
+
+          // XXX: check https://docs.meteor.com/api/connections.html
+            //able to get the connections info
+          // TODO: Let's get a good grasp on how to create new templates
+          // TODO: and then DBs
           // TODO: check https://github.com/artwells/meteor-accounts-guest
