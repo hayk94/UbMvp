fa99005cbb6cd7ad5e9d3c528c59d9275c6cbd3b
Hayk <hayk94@gmail.com>
2016-08-16 11:31:09 +0400

    successfully printing the connection info from db to the Template only the headers part is left

--- server/main.js
+++ server/main.js
@@ -1,15 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/tasks.js';
-import '../imports/api/conns.js';
+import { Conns } from  '../imports/api/conns.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
   Meteor.onConnection(function(conn){
-      // console.log(conn);
-      connInfoS.insert({
+      console.log(conn);
+      Conns.insert({
         connID: conn.id,
         ipAdr: conn.clientAddress,
         httpHeads: conn.httpHeaders,
       });
+    });
 });
