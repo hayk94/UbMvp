c39d606cea8f5146ba9a5415c3335c19bc282094
Hayk <hayk94@gmail.com>
2016-08-17 15:21:07 +0400

    successfully created the db and insert an ip into it once

--- server/main.js
+++ server/main.js
@@ -2,20 +2,55 @@ import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/tasks.js';
 import { Conns } from  '../imports/api/conns.js';
+import { Ips } from '../imports/api/ips.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
   Meteor.onConnection(function(conn){
-      console.log(conn);
-      Conns.insert({
-        connID: conn.id,
-        ipAdr: conn.clientAddress,
-        httpHeads: {
-          host: conn.httpHeaders.host,
-          userAgent:conn.httpHeaders['user-agent'],
-          realIP: conn.httpHeaders['x-real-ip'],
-        },
-        createdAt: new Date(),
-      });
-    });
-});
+    var connID = conn.id ;
+    var ipAdr = conn.clientAddress ;
+    var realIP = conn.httpHeaders['x-real-ip'] ;
+
+    //checking whether we are getting the right ips
+    if (ipAdr !== realIP) {
+      if (!realIP) {
+        console.log('You are running locally');
+      }
+      console.log('You forget HTTP_FORWARDED_COUNT="1" or put the wrong number of proxies');
+    }
+
+    //check if the ip is registered
+    console.log(Ips.findOne({"ipAdr":ipAdr}));
+    if (!Ips.findOne({"ipAdr":ipAdr})) {
+
+        Ips.insert({
+          ipAdr:ipAdr,
+          connections: [{
+            connID: conn.id,
+            ipAdr: conn.clientAddress,
+            httpHeads: {
+              host: conn.httpHeaders.host,
+              userAgent:conn.httpHeaders['user-agent'],
+              realIP: conn.httpHeaders['x-real-ip'],
+            },
+            createdAt: new Date(),
+          }],//connections
+          createdAt: new Date(),
+        });//Ips.insert
+      //
+      console.log(); console.log();
+    }
+
+      // console.log(conn);
+      // Conns.insert({
+      //   connID: conn.id,
+      //   ipAdr: conn.clientAddress,
+      //   httpHeads: {
+      //     host: conn.httpHeaders.host,
+      //     userAgent:conn.httpHeaders['user-agent'],
+      //     realIP: conn.httpHeaders['x-real-ip'],
+      //   },
+      //   createdAt: new Date(),
+      // });
+    });// Meteor onConnection
+});//Meteor startup
