d01dba82afbf10030dd52975f1e59a8e8ad0149b
Hayk <hayk94@gmail.com>
2016-08-17 10:55:02 +0400

    added the allConns to the db

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
+import { Conns } from  '../imports/api/conns.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
   allConns = Meteor.default_server.stream_server.all_sockets();
@@ -15,8 +17,12 @@ Meteor.startup(() => {
   console.log(allConns);
   Meteor.onConnection(function(conn){
     // console.log(allConns);
-    //The array above is to big for the console let's convert it to string
-
+    //The array above is too big for the console let's convert it to string
+    var allConsStr = objToString(allConns);
+    Conns.insert({
+            allConsStr,
+            createdAt: new Date(),
+          });
     //  console.log(conn);
     // var userObject = {
     //   username: "testMe",
