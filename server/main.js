5a3e2c2c3add86f0d2658e935fd4f300656cb185
Hayk <hayk94@gmail.com>
2016-08-17 10:35:34 +0400

    func objToString

--- server/main.js
+++ server/main.js
@@ -2,10 +2,21 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  console.log(Meteor.default_server.stream_server.all_sockets());
+  allConns = Meteor.default_server.stream_server.all_sockets();
+    function objToString (obj) {
+      var str = '';
+      for (var p in obj) {
+          if (obj.hasOwnProperty(p)) {
+              str += p + '::' + obj[p] + '\n';
+          }
+      }
+      return str;
+    }
+  console.log(allConns);
   Meteor.onConnection(function(conn){
-    console.log(Meteor.default_server.stream_server.all_sockets());
-    
+    // console.log(allConns);
+    //The array above is to big for the console let's convert it to string
+
     //  console.log(conn);
     // var userObject = {
     //   username: "testMe",
