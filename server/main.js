e29f7e79eff8a8786d47d19134085cdb156a697d
Hayk <hayk94@gmail.com>
2016-08-17 10:05:14 +0400

    getting all the active connections

--- server/main.js
+++ server/main.js
@@ -2,7 +2,10 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  console.log(Meteor.default_server.stream_server.all_sockets());
   Meteor.onConnection(function(conn){
+    console.log(Meteor.default_server.stream_server.all_sockets());
+    
     //  console.log(conn);
     // var userObject = {
     //   username: "testMe",
