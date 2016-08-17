af7957eccbede755f619892972ce1e5f524b15c3
Hayk <hayk94@gmail.com>
2016-08-17 13:43:30 +0400

    just comparing the sockets connections with the meteor.onConnection

--- server/main.js
+++ server/main.js
@@ -16,10 +16,18 @@ Meteor.startup(() => {
     }
   console.log(allConns);
   Meteor.onConnection(function(conn){
+    allConns = Meteor.default_server.stream_server.all_sockets();
     console.log(allConns);
     //The array above is too big for the console let's convert it to string
-    var allConsStr = objToString(Meteor.default_server.stream_server.all_sockets());
+
+    var allConsStr = objToString(allConns); // this returns a string with the unique id
+      // var allConsStr = JSON.stringify(Meteor.default_server.stream_server.all_sockets());
+      // var allConsStr =allConns.j;
+      var currConStr = objToString(conn);
     console.log(allConsStr);
+    console.log('Current CONN : ', conn);
+    console.log('Curr CONN STR : ',currConStr);
+    console.log('Current CONN END');
     Conns.insert({
             allConsStr,
             createdAt: new Date(),
