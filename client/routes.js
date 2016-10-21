2ea681ab2e13b4e8c403f41cb9d3d71985d2fd7d
Hayk <hayk94@gmail.com>
2016-10-21 14:22:17 +0400

    removed comments

--- client/routes.js
+++ client/routes.js
@@ -6,11 +6,6 @@ FlowRouter.route('/',{
   }
 });
 
-// FlowRouter.route('/:postId',{
-//   action: function () {
-//     BlazeLayout.render('body', {content:"blogPost"});
-//   }
-// })
 FlowRouter.route('/ips',{
   action: function () {
     BlazeLayout.render('mainLayout', {content:"ips"});
@@ -28,14 +23,11 @@ Tracker.autorun(function() {
     FlowRouter.watchPathChange();
     var context = FlowRouter.current();
     // use context to access the URL state
-    // console.log(window.location.href);
     console.log(context);
-    // console.log(window.location.hash);
     var visitedOne = context.path;
-    // console.log(window.location.href);
-    //getting the connID
 
-    var clientIp = headers.getClientIP(); // no need for this anymore
+    //getting the connID
+    var clientIp = headers.getClientIP();
     var clientConnId = Meteor.connection._lastSessionId;
     console.log(clientIp);
     console.log(clientConnId);
