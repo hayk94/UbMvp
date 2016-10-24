e82389b60e92f377e01855725b26ae8b242bbba0
Hayk <hayk94@gmail.com>
2016-10-24 14:36:43 +0400

    initial Client Component

---
+++ imports/ui/Client.jsx
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react'
+import { Meteor } from 'meteor/meteor'
+import classnames from 'classnames'
+
+export default class Client extends Component {
+  render () {
+    
+  } // render
+} // Client
