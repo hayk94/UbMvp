f2ecb2ef78fdf15eca0454460a05e0a2329f706d
Hayk <hayk94@gmail.com>
2016-11-03 16:45:46 +0400

    minor changes

--- imports/ui/Client.jsx
+++ imports/ui/Client.jsx
@@ -6,7 +6,7 @@ export default class Client extends Component {
   render () {
     // DONE:0 make an array from the ips but with the needed info and more user-oriented ... No need for this
 
-    return <p>{this.props.ip.ipAdr}</p>
+    return <div className="contactBox">{this.props.ip.ipAdr}</div>
   } // render
 } // Client
 
