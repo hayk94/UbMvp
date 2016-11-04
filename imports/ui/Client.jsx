bdb88f4442bd09c2f6d781c2fca14c8ca17e4e6e
Hayk <hayk94@gmail.com>
2016-11-04 14:39:40 +0400

    no need for these imports

--- imports/ui/Client.jsx
+++ imports/ui/Client.jsx
@@ -1,9 +1,9 @@
-/*=============================================>>>>>
+/* =============================================>>>>>
 = imports =
-===============================================>>>>>*/
+===============================================>>>>> */
 import React, { Component, PropTypes } from 'react'
-import { Meteor } from 'meteor/meteor'
-import classnames from 'classnames'
+// import { Meteor } from 'meteor/meteor'
+// import classnames from 'classnames'
 
 /* ----------- Components ----------- */
 
