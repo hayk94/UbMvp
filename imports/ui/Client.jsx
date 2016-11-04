04d0aca6f327aa61156bb2a1d8aeaed951f69fa2
Hayk <hayk94@gmail.com>
2016-11-04 10:40:58 +0400

    WIP make new array with same vid

--- imports/ui/Client.jsx
+++ imports/ui/Client.jsx
@@ -5,7 +5,8 @@ import classnames from 'classnames'
 export default class Client extends Component {
   render () {
     // DONE:0 make an array from the ips but with the needed info and more user-oriented ... No need for this
-
+    var contactArr = this.props.ip.connections.map((vid) => ({vid}))
+    console.log(contactArr)
     return <div className="contactBox">{this.props.ip.ipAdr}</div>
   } // render
 } // Client
