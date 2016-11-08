46538fe5b97cbac7398d4a3dc29be2db1f4181cb
Hayk <hayk94@gmail.com>
2016-11-08 14:47:38 +0400

    some comments

--- client/main.jsx
+++ client/main.jsx
@@ -5,6 +5,6 @@ import { render } from 'react-dom'
 import App from '../imports/ui/App.jsx'
 
 Meteor.startup(() => {
-  _ = lodash
+  _ = lodash // no need to import lodash it is defined globally
   render(<App />, document.getElementById('render-target'))
 })
